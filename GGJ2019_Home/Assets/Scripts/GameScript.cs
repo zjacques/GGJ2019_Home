f2e496447f5feceb8e11ed9452372eb8eeafa0ae
zjacques <zachary.jacques160@gmail.com>
2019-01-26 20:09:21 +0000

    added timer to game script in dj scene

--- GGJ2019_Home/Assets/Scripts/GameScript.cs
+++ GGJ2019_Home/Assets/Scripts/GameScript.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
     public Sprite[] sprites;
+    public Text timerText;
+    public float gameTime = 60;
+    public bool gameStarted = true;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +19,30 @@ public class GameScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameStarted)
+        {
+            gameTime -= Time.deltaTime;
+            if(gameTime <= 0)
+            {
+                gameTime = 0;
+                gameStarted = false;
+            }
+            timerText.text = Mathf.Floor(gameTime).ToString();
+            if(gameStarted == false)
+            {
+                EndGame();
+            }
+        }
+    }
+
+    void StartGame(){
+        gameStarted = false;
+        gameTime = 60;
+        timerText.text = "60";
+    }
+
+    void EndGame(){
+        //show scoreboard stuff
+        timerText.text = "";
     }
 }
